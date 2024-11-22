949f29d979791cb9e8dcd581b9e1ccd80fbaeb44
Jack <105053344+meqte@users.noreply.github.com>
2024-11-22 20:05:31 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -16,7 +16,7 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/10.0.10/10.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/10.0..0.10/10.0.0.1/g' package/base-files/files/bin/config_generate
 #
 # ------------------------------- Main source ends -------------------------------
 
@@ -24,6 +24,10 @@ sed -i 's/10.0.10/10.0.1/g' package/base-files/files/bin/config_generate
 #
 # Add luci-app-amlogic
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall2 luci-app-passwall2
+
+
 
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
